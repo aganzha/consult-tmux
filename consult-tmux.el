4320bb0c1a2789e2a36b52954e4d3a9abce80c06
au.ganzha <au.ganzha@tensor.ru>
2026-08-10 17:24:08 +0200

    added copyright

--- consult-tmux.el
+++ consult-tmux.el
@@ -1,5 +1,30 @@
 ;;; consult-tmux.el --- Connect to tmux sessions from Emacs via consult -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2026 Aleksey Ganzha
+
+;; Author: Aleksey Ganzha <aganzha@yandex.ru>
+;; URL https://github.com/aganzha/consult-tmux
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "30.2"))
+;; Keywords: convenience, consult, tmux
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; Keywords: convenience, consult, tmux
+
 ;;; Commentary:
 ;; Provides `consult-tmux', which lists active tmux sessions in the
 ;; minibuffer (consult/vertico) and attaches to the chosen one in a
